7047e9f9b96c2d5fe82315eb8859730c565ffe3c
Igor Nalewajko <igor@MacBook-Pro-Igor.local>
2026-06-09 12:16:24 +0200

    Udostępnianie API, Wdrozenie aplikacji na produkcji, Poprawki

--- Sklep.Core/Interfaces/IProductRepository.cs
+++ Sklep.Core/Interfaces/IProductRepository.cs
@@ -7,4 +7,14 @@ public interface IProductRepository : IBaseRepository<Product>
     Task<IEnumerable<Product>> GetAllWithCategoryAsync();
     Task<Product?> GetByIdWithCategoryAsync(int id);
     void RemoveImage(ProductImage image);
+    
+    Task<IEnumerable<Product>> GetFilteredProductsAsync(string? categoryName, string? searchString, int? sizeId, string? color);
+    Task<IEnumerable<Size>> GetAvailableSizesAsync();
+    Task<IEnumerable<string>> GetAvailableColorsAsync();
+    Task<bool> IsProductInWishlistAsync(int productId, string userId);
+    Task AddProductViewHistoryAsync(ProductViewHistory history);
+    Task AddReviewAsync(Review review);
+    Task<Review?> GetReviewByIdAsync(int reviewId);
+    void DeleteReview(Review review);
+    Task AddQuestionAsync(ProductQuestion question);
 }
